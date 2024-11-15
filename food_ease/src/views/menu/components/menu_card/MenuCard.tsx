2f83710c028d76f8fed357257db62fbac5afc640
Jennifer Ardelia Limicia <jennifer.limicia@binus.edu>
2024-11-15 19:59:12 +0700

    edit menu image

--- food_ease/src/views/menu/components/menu_card/MenuCard.tsx
+++ food_ease/src/views/menu/components/menu_card/MenuCard.tsx
@@ -9,13 +9,17 @@ const MenuCard = (props: IMenu) => {
     return (
       <>
         {menu &&
-          <div className='w-full'>
-            <img src={"https://utfs.io/f/" + menu.imageKey} alt="" />
-            <div className='flex justify-between items-center'>
-                <p className="px-5 pt-5 font-bold text-xl">{menu.name}</p>
-                <p className='text-[#FFB0B0] px-5 pt-5 font-bold text-xl'>{menu.price/1000} K</p>
+          <div className='w-full flex justify-center flex-col items-center mt-3'>
+            <div className='flex items-center w-[95%]'>
+                <img src={"https://utfs.io/f/" + menu.imageKey} alt="" className='rounded-3xl'/>
             </div>
-            <p className="px-5 text-base">{menu.description}</p>
+            <div className='px-5 pt-3 flex justify-between w-full flex-col'>
+                <div className='flex justify-between items-center w-full'>
+                    <p className="font-bold text-xl">{menu.name}</p>
+                    <p className='text-[#FFB0B0] font-bold text-xl'>{menu.price/1000} K</p>
+                </div>
+                <p className="text-base">{menu.description}</p>
+            </div>
           </div> 
         }
         <hr className='bg-[#FFD09B] my-[7%] mx-5 h-[0.8%]' />
